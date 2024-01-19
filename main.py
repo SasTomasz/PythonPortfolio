e7abe94793c146d9ccc72ac79b1689943ee22acd
tomasz_sas <tomaszsas.dev@gmail.com>
2024-01-19 11:28:47 +0100

    Adding logger, draw title from csv in two separate columns

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas
+import logger_utils
+
+logger = logger_utils.logger
 
 st.set_page_config(layout="wide")
 
@@ -16,3 +20,18 @@ with col2:
 
 contact_message = "Below you can find some of the apps I have build in Python. Feel free to contact me!"
 st.write(contact_message)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('./data.csv', sep=';')
+half_of_rows = len(df.axes[0]) // 2
+logger.debug((f"DF\n__________________________\n {df.columns}"))
+
+
+with col3:
+    for index, row in df[:half_of_rows].iterrows():
+        st.title(row['title'])
+
+with col4:
+    for index, row in df[half_of_rows:].iterrows():
+        st.title(row['title'])
