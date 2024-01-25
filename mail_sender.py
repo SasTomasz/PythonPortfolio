9bf99af133cb06c857de75dccae161ba4934f82f
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-01-25 08:10:39 +0100

    Implementing sending email from web-form

--- mail_sender.py
+++ mail_sender.py
@@ -12,28 +12,34 @@ logger = logger_utils.logger
 
 host = 'smtp.gmail.com'
 port = 587
-login = os.getenv('LOGIN')
 password = os.getenv('APP_PASSWORD')
+email_sender = os.getenv('LOGIN')
 email_receiver = os.getenv('MY_EMAIL')
 message = """\
 Subject: Hello from python
-This is my first message sending with python code
+This is a message sending with python code
 """
 
 context = ssl.create_default_context()
 
 
-def send_email(sender, receiver):
+def send_email():
     try:
         server = smtplib.SMTP(host, port)
         server.starttls(context=context)
-        server.login(login, password)
-        server.sendmail(sender, receiver, message)
+        server.login(email_sender, password)
+        server.sendmail(email_sender, email_receiver, message)
+        server.quit()
+        return True
     except Exception as e:
         logger.error(e)
-    finally:
-        server.quit()
+        return False
+
+
+def set_email_content(user_email, user_message):
+    global message
+    message = f"Subject: web-form from {user_email}\n{user_message}"
 
 
 if __name__ == "__main__":
-    send_email(login, email_receiver)
+    send_email()
--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,8 +1,23 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'>Contact me</h1>", unsafe_allow_html=True)
+import mail_sender
 
-with st.form("contact_form"):
+mail_was_sent_message = "Your message was sent successfully"
+mail_was_not_sent_message = ("There were a problem with sending Your email. "
+                             "Try again or try it later")
+
+st.markdown("<h1 style='text-align: center;'>Contact me</h1>",
+            unsafe_allow_html=True)
+
+with st.form("contact_form", clear_on_submit=True):
     email = st.text_input("Your email address")
     message = st.text_area("Your message")
-    st.form_submit_button("Submit")
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        mail_sender.set_email_content(email, message)
+        is_sent = mail_sender.send_email()
+        if is_sent:
+            st.write(mail_was_sent_message)
+        else:
+            st.write(mail_was_not_sent_message)
